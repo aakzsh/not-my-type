6d84204c4702040df403e5b7517ad93e8c4cfea6
aakzsh <aakashferrari@gmail.com>
2022-01-08 19:41:41 +0530

    add frontend part

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template, request, redirect
+import random
+import string
+import base64
+import os
+
+from flask.typing import StatusCode
+from twiliosend import send
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -6,7 +14,24 @@ app.config["DEBUG"] = True
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    res = str(''.join(random.choices(string.ascii_lowercase + string.digits, k = 15)))
+    return render_template('index.html', code=res)
+
+@app.route('/create/<code>')
+def create(code):
+    # lol = str(base64.b64decode(name))[2:-1]
+    # lol = lol[0:int(len(lol)/2)]
+    # print(lol)
+    return render_template('create.html', code=code)
+
+
+@app.route('/play')
+def play():
+    return render_template('play.html')
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ twiliosend.py
@@ -0,0 +1,19 @@
+import os
+from twilio.rest import Client
+
+
+# account_sid = os.environ['TWILIO_ACCOUNT_SID']
+# auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
+def send(email):
+    # client = Client(account_sid, auth_token)
+    # verification = client.verify \
+    #                  .services('VAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') \
+    #                  .verifications \
+    #                  .create(to=email, channel='email')
+
+    # print(verification.sid)
+    print("hello")
+
+
+    
